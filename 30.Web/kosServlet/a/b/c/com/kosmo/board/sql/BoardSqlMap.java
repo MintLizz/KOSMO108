8bd9d1ef2934ac5cf9b5ee2c6a723b3a06545597
MintLizz <103358721+MintLizz@users.noreply.github.com>
2022-04-28 18:15:58 +0900

    Add files via upload

--- 30.Web/kosServlet/a/b/c/com/kosmo/board/sql/BoardSqlMap.java
+++ 30.Web/kosServlet/a/b/c/com/kosmo/board/sql/BoardSqlMap.java
@@ -2,9 +2,63 @@ package a.b.c.com.kosmo.board.sql;
 
 public abstract class BoardSqlMap {
 	
-	public static String getBoardSelectAll(){return null;}
+	public static String getBoardSelectAll(){
+		
+		/*
+		  SELECT 
+		     A.BNUM         BNUM
+		    ,A.BSUBJECT     BSUBJECT
+		    ,A.BWRITER      BWRITER 
+		    ,A.BPW          BPW
+		    ,A.BMEMO        BMEMO
+		    ,A.DELETEYN     BDELETEYN
+		    ,TO_CHAR(A.INSERTDATE,'YYYY-MM-DD')   INSERTDATE
+		    ,TO_CHAR(A.UPDATEDATE,'YYYY-MM-DD')   UPDATEDATE
+		FROM MVC_BOARD A
+		WHERE A.DELETEYN = 'Y'
+		
+		ORDER BY 1 DESC;
+		  
+		  */
+		
+		StringBuffer sb1 = new StringBuffer();
+		
+		sb1.append("SELECT														\n"); 
+		sb1.append("		A.BNUM         							BNUM		\n");
+		sb1.append("		,A.BSUBJECT     						BSUBJECT	\n");
+		sb1.append("		,A.BWRITER      						BWRITER 	\n");
+		sb1.append("		,A.BPW          						BPW			\n");	
+		sb1.append("		,A.BMEMO        						BMEMO		\n");
+		sb1.append("		,A.DELETEYN     						DELETEYN	\n");
+		sb1.append("    	,TO_CHAR(A.INSERTDATE,'YYYY-MM-DD')  	INSERTDATE	\n");
+		sb1.append("		,TO_CHAR(A.UPDATEDATE,'YYYY-MM-DD')   	UPDATEDATE	\n");
+		sb1.append("FROM 	MVC_BOARD A											\n");
+		sb1.append("WHERE 	A.DELETEYN = 'Y'									\n");
+		sb1.append("ORDER BY 1 DESC											\n");
+		
+		return sb1.toString();
+		
+	}
 	
-	public static String getBoardSelect(){ return null;}
+	public static String getBoardSelect(){ 
+		
+		StringBuffer sb2 = new StringBuffer();
+		
+		sb2.append("SELECT												\n");		
+		sb2.append("A.BNUM         							BNUM		\n");
+		sb2.append(",A.BSUBJECT     						BSUBJECT	\n");
+		sb2.append(",A.BWRITER      						BWRITER 	\n");
+		sb2.append(",A.BPW          						BPW			\n");
+		sb2.append(",A.BMEMO        						BMEMO		\n");
+		sb2.append(",A.DELETEYN     						DELETEYN	\n");
+		sb2.append(",TO_CHAR(A.INSERTDATE,'YYYY-MM-DD')  	INSERTDATE	\n");
+		sb2.append(",TO_CHAR(A.UPDATEDATE,'YYYY-MM-DD')   	UPDATEDATE	\n");
+		sb2.append("FROM 	MVC_BOARD A									\n");
+		sb2.append("WHERE 	A.DELETEYN = 'Y'							\n");
+		sb2.append("AND A.BNUM = ?										\n");
+		
+		return sb2.toString();
+		}
 	
 	public static String getBoardInsert(){ 
 		
@@ -44,7 +98,22 @@ public abstract class BoardSqlMap {
 		
 	}
 	
-	public static String getBoardUpdate(){ return null;}
+	// Update 
+	public static String getBoardUpdate(){ 
+		
+		
+		StringBuffer sb4 = new StringBuffer();
+		
+		sb4.append("UPDATE      MVC_BOARD						\n");
+		sb4.append("SET         BSUBJECT	=	?,				\n");
+		sb4.append("			BMEMO		=	?,				\n");
+		sb4.append("			UPDATEDATE 	= 	SYSDATE			\n");
+		sb4.append("WHERE       DELETEYN 	= 	'Y'				\n");
+		sb4.append("AND         BNUM 		= 	?				\n");		
+		
+		return sb4.toString();
+		
+	}
 	
 	public static String getBoardDelete(){ return null;}
 
